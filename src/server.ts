702e28862e81ac1f9b3ebe96970cd7c6af48b970
Eloy Simonin Genre <simonineloy123@gmail.com>
2026-03-07 22:58:42 -0300

    fix: Se agrega dominio de CORS para produccion

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://gestor-passwords-frontend.vercel.app'
+  ],
   credentials: true,
 }));
 
